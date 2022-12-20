2674727bb908a87b3fc47a2c56ac190710ce22dc
nick <nick@evolution.com>
2022-12-20 19:50:36 +0800

    install postgresal db adapter

---
+++ app/app/calc.py
@@ -0,0 +1,27 @@
+"""
+Calculator functions
+"""
+
+def add(x, y):
+    """Return the sum of the two argument
+
+    Args:
+        x (Number): number1
+        y (Number): number2
+
+    Returns:
+        Number: sum of number1 and number2
+    """
+    return x + y
+
+def subtract(x, y):
+    """Return the result of x subtract y
+
+    Args:
+        x (Number): 减数
+        y (Number): 被减数
+
+    Returns:
+        Number: x - y 的结果
+    """
+    return x - y
